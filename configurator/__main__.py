b5783bb5eb00d9d96446e2a31819cc67dbd14a5b
Mark Dietzer <git@doridian.net>
2020-03-01 15:33:10 -0800

    Add configurator

---
+++ configurator/__main__.py
@@ -0,0 +1,149 @@
+from yaml import load as yaml_load, dump as yaml_dump
+from os import listdir, path, unlink, rename, system, mkdir
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from requests import get as http_get
+from zipfile import ZipFile
+from io import BytesIO
+from shutil import rmtree
+
+__dir__ = path.dirname(__file__)
+
+DIR = path.abspath(path.join(__dir__, "sites"))
+OUTDIR = path.abspath(path.join(__dir__, "out"))
+OLDDIR = path.abspath(path.join(OUTDIR, "sites"))
+
+SITEDIR = '/var/www/sites'
+
+j2env = Environment(
+    loader=FileSystemLoader(path.join(__dir__, 'configs')),
+    autoescape=select_autoescape([])
+)
+nginxSiteTemplate = j2env.get_template('nginx/site.conf.j2')
+nginxMainTemplate = j2env.get_template('nginx/main.conf.j2')
+
+def loadSiteNoop(site, oldSite, force):
+    return False
+
+def loadSiteZIP(site, oldSite, force):
+    if oldSite and not force and site['src'] == oldSite['src']:
+        return False
+    outDir = path.join(SITEDIR, site['name'])
+    site['dir'] = outDir
+
+    newDir = "%s___new" % outDir
+    oldDir = "%s___old" % outDir
+    
+    rmtree(newDir, ignore_errors=True)
+    rmtree(oldDir, ignore_errors=True)
+    rmtree(newDir, ignore_errors=True)
+
+    mkdir(newDir)
+    r = http_get(site['src'], stream=True)
+    z = ZipFile(BytesIO(r.content))
+    z.extractall(newDir)
+
+    try:
+        rename(outDir, oldDir)
+    except FileNotFoundError:
+        pass
+    rename(newDir, outDir)
+    rmtree(oldDir, ignore_errors=True)
+
+    return False
+
+def swapFile(file, content):
+    newfile = "%s.new" % file
+    fh = open(newfile, "w")
+    fh.write(content)
+    fh.close()
+
+    try:
+        unlink(file)
+    except FileNotFoundError:
+        pass
+    rename(newfile, file)
+
+    return False # TODO: Return if changed
+
+SITE_LOADERS = {
+    'redirect': loadSiteNoop,
+    'none': loadSiteNoop,
+    'zip': loadSiteZIP
+}
+
+def run():
+    nginxConfig = [nginxMainTemplate.render()]
+    certifierConfig = []
+    reloadNginx = False
+    reloadCertifier = False
+
+    files = listdir(DIR)
+
+    for file in files:
+        if file[0] == '.' or file[-4:] != '.yml':
+            continue
+
+        curName = path.join(DIR, file)
+        oldName = path.join(OLDDIR, file)
+
+        fh = open(curName, 'r')
+        site = yaml_load(fh)
+        fh.close()
+
+        site['name'] = file[:-4]
+
+        oldSite = None
+        try:
+            fh = open(oldName, 'r')
+            oldSite = yaml_load(fh)
+            fh.close()
+        except:
+            pass
+
+        if not oldSite:
+            oldSite = {
+                'domains': [],
+                'type': 'none',
+            }
+            reloadNginx = True
+            reloadCertifier = True
+        
+        oldSite['name'] = site['name']
+
+        typeChanged = site['type'] != oldSite['type']
+        reloadNginx |= typeChanged
+        reloadCertifier |= typeChanged
+
+        loader = SITE_LOADERS[site['type']]
+        if loader(site, oldSite, typeChanged):
+            reloadNginx = True
+
+        doms = site['domains']
+        oldDoms = oldSite['domains']
+        reloadCertifier |= len(oldDoms) != len(doms)
+        if not reloadCertifier:
+            for dom, idx in site['domains']:
+                if dom != oldDoms[idx]:
+                    reloadCertifier = True
+                    break
+
+        certifierConfig.append(site)
+        nginxConfig.append(nginxSiteTemplate.render(site=site))
+
+
+    certifierConfStr = yaml_dump(certifierConfig)
+    nginxConfStr = '\n'.join(nginxConfig)
+
+    if swapFile("/etc/nginx/conf.d/cdn.conf", nginxConfStr):
+        reloadNginx = True
+
+    if swapFile(path.join(OUTDIR, "certifier.yml"), certifierConfStr):
+        reloadCertifier = True
+
+    if reloadNginx:
+        system('service nginx reload')
+
+    #if reloadCertifier:
+    #    system('service certifier restart')
+
+run()
