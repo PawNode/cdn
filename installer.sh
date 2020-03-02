5cf92efe70f85a83778021fa5359d23dddc0949d
Mark Dietzer <git@doridian.net>
2020-03-02 15:16:38 -0800

    Things

--- installer.sh
+++ installer.sh
@@ -2,8 +2,12 @@
 set -euo pipefail
 
 cd "$(dirname "$0")"
+
 git pull
+
 if [ ! -f /etc/ssl/default.crt ]
 then
     openssl req -newkey rsa:4096 -nodes -keyout /etc/ssl/default.key -x509 -days 1 -out /etc/ssl/default.crt
 fi
+
+python3 ./configurator
