7af3c6733fa20c27c94392f86f782898641b989b
Mark Dietzer <git@doridian.net>
2020-03-02 15:01:09 -0800

    from

--- configurator/ipsetter.sh
+++ configurator/ipsetter.sh
@@ -2,4 +2,4 @@
 cd "$(dirname "$0")" || exit 1
 
 cat out/ips.txt | xargs -n 1 ip addr add dev lo
-cat out/ip_rules.txt | xargs -n 1 ip rule add lookup 666 src
+cat out/ip_rules.txt | xargs -n 1 ip rule add lookup 666 from
