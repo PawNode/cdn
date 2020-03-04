c99200eaad1d347fdab728696357d93554330e92
Mark Dietzer <git@doridian.net>
2020-03-03 16:38:23 -0800

    dnsseckey

--- certifier/__main__.py
+++ certifier/__main__.py
@@ -2,6 +2,7 @@ from myacme import get_ssl_for_site
 from yaml import load as yaml_load
 from myglobals import __dir__
 from os import path, system
+from loader import loadFile, storeFile
 
 sites = []
 with open(path.join(__dir__, 'sites.yml'), 'r') as f:
@@ -10,6 +11,11 @@ with open(path.join(__dir__, 'sites.yml'), 'r') as f:
 reloadNginx = False
 for site in sites:
     reloadNginx |= get_ssl_for_site(site)
+
+loadFile("dnssec/Kcdn.pawnode.com.+013+27208.key")
+loadFile("dnssec/Kcdn.pawnode.com.+013+27208.private")
+loadFile("dnssec/Kcdn.pawnode.com.+013+51197.key")
+loadFile("dnssec/Kcdn.pawnode.com.+013+51197.private")
 
 if reloadNginx:
     system('service nginx reload')
