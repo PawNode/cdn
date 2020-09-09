988f6d6070dc3d551e366245199525266c5e7e62
Mark Dietzer <git@doridian.net>
2020-09-09 09:12:00 -0700

    Add pip

--- installer.sh
+++ installer.sh
@@ -16,7 +16,7 @@ addIfMissing() {
     fi
 }
 
-apt-get -y install pdns-server pdns-backend-bind nginx python3 python3-acme python3-boto3 python3-josepy python3-jinja2 python3-pycryptodome bird apparmor-utils sudo git gcc libfuse-dev fuse bind9utils software-properties-common at python3-dnspython python3-requests
+apt-get -y install pdns-server pdns-backend-bind nginx python3 python3-acme python3-boto3 python3-josepy python3-jinja2 python3-pycryptodome bird apparmor-utils sudo git gcc libfuse-dev fuse bind9utils software-properties-common at python3-dnspython python3-requests python3-pip
 pip3 install dyndbmutex
 
 enableStart() {
